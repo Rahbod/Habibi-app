7bae6287e779477b8025afb977468fd41b2963db
behnam8871 <bgh1371@gmail.com>
2018-10-22 20:56:24 +0330

    start

--- app/src/main/java/ir/rahbod/habibi/pages/MainActivity.java
+++ app/src/main/java/ir/rahbod/habibi/pages/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "rr", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
