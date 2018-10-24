ea2f7fc97bada46b41c0231d204549e7546c9dd5
behnam8871 <bgh1371@gmail.com>
2018-10-24 10:36:02 +0330

    finish register

---
+++ app/src/main/java/ir/rahbod/habibi/pages/UserNameActivity.java
@@ -0,0 +1,15 @@
+package ir.rahbod.habibi.pages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.rahbod.habibi.R;
+
+public class UserNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_name);
+    }
+}
