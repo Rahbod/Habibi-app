bb52a51087c2ed9baf2eabe4642cadfb063ef87c
behnam8871 <bgh1371@gmail.com>
2018-12-19 14:27:34 +0330

    add fabric

--- app/src/main/java/ir/rahbod/habibi/controller/MyApp.java
+++ app/src/main/java/ir/rahbod/habibi/controller/MyApp.java
@@ -2,12 +2,15 @@ package ir.rahbod.habibi.controller;
 
 import android.app.Application;
 import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MyApp extends Application {
     public static volatile Context context;
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         context = getApplicationContext();
     }
 }
